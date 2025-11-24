98bc91f404d3835f389a444e0fefee2ad35cdced
Mohamed Hamouch <med10hamouch@gmail.com>
2025-11-24 14:03:48 +0100

    Add Payment entity

---
+++ src/main/java/ma/microtech/smartShop/entities/Payment.java
@@ -0,0 +1,70 @@
+package ma.microtech.smartShop.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import ma.microtech.smartShop.entities.enums.PaymentStatus;
+import ma.microtech.smartShop.entities.enums.PaymentType;
+
+@Entity
+@Table(name = "payments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Order order;
+
+    @Column(nullable = false)
+    private Integer paymentNumber;
+
+    @Column(nullable = false)
+    private String reference;
+
+    @Column(nullable = false)
+    private Double amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentType type;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentStatus status;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private LocalDateTime paymentDate = LocalDateTime.now();
+
+    @Column
+    private LocalDateTime encashmentDate;
+
+    @Column
+    private String bank;
+}
