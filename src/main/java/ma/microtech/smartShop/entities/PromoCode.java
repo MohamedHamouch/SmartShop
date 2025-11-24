bb76679cb3930b5a98ce3d86ea563580daf7d7bf
Mohamed Hamouch <med10hamouch@gmail.com>
2025-11-24 12:18:22 +0100

    Add PromoCode entity

---
+++ src/main/java/ma/microtech/smartShop/entities/PromoCode.java
@@ -0,0 +1,28 @@
+package ma.microtech.smartShop.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "promo_codes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PromoCode {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String code;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Boolean active = true;
+
+    @Column(nullable = false)
+    private Double discountPercentage;
+}
