e1f216397c1eb22698ad57a35aef18b68745480a
Mohamed Hamouch <med10hamouch@gmail.com>
2025-11-24 12:07:29 +0100

    Add User entity

---
+++ src/main/java/ma/microtech/smartShop/entities/User.java
@@ -0,0 +1,37 @@
+package ma.microtech.smartShop.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import ma.microtech.smartShop.entities.enums.UserRole;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Client client;
+}
