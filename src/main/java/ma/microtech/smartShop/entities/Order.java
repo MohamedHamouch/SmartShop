10fe7140064d8dd5518dadc0cb0081f0533bd6da
Mohamed Hamouch <med10hamouch@gmail.com>
2025-11-24 12:22:37 +0100

    Add Order entity

---
+++ src/main/java/ma/microtech/smartShop/entities/Order.java
@@ -0,0 +1,95 @@
+package ma.microtech.smartShop.entities;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import ma.microtech.smartShop.entities.enums.OrderStatus;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Client client;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column
+    private LocalDateTime confirmedAt;
+
+    @Column(nullable = false)
+    private Double subtotal;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Double discount = 0.0;
+
+    @Column(nullable = false)
+    private Double tvaRate;
+
+    @Column(nullable = false)
+    private Double tva;
+
+    @Column(nullable = false)
+    private Double total;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Builder.Default
+    private OrderStatus status = OrderStatus.PENDING;
+
+    @Column(nullable = false)
+    private Double remainingAmount;
+
+    @ManyToOne
+    @JoinColumn(name = "promo_code_id")
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private PromoCode promoCode;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private List<OrderItem> items = new ArrayList<>();
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private List<Payment> payments = new ArrayList<>();
+}
