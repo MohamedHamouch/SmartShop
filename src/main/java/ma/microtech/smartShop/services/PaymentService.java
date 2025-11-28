57b1c4d8dea32bafc6ecf23fa54624d39d009af2
Mohamed Hamouch <med10hamouch@gmail.com>
2025-11-28 15:54:28 +0100

    Add PaymentService interface

---
+++ src/main/java/ma/microtech/smartShop/services/PaymentService.java
@@ -0,0 +1,17 @@
+package ma.microtech.smartShop.services;
+
+import java.util.List;
+
+import ma.microtech.smartShop.dto.request.PaymentRequest;
+import ma.microtech.smartShop.dto.response.PaymentResponse;
+
+public interface PaymentService {
+
+    PaymentResponse recordPayment(PaymentRequest request);
+
+    PaymentResponse encashPayment(Long paymentId);
+
+    PaymentResponse rejectPayment(Long paymentId);
+
+    PaymentResponse findById(Long paymentId);
+}
